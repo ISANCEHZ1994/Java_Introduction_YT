0fa95fe5be5d128a64c11114497928b569247dbc
Israel <Israel.pro1994@gmail.com>
2020-12-23 11:18:33 -0600

    adding notes

--- HelloWorld/src/com/codewithisrael/Main.java
+++ HelloWorld/src/com/codewithisrael/Main.java
@@ -1,8 +1,13 @@
 package com.codewithisrael;
 
-public class Main {
+public class Main { // this is a Class with the name of Main - also a part of the package ABOVE
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // this is a Function that is PART of the Main Class
+        // the parentheses is a PARAMETER for the function that allows us to pass values to our programs (like in JS duh)
+        // NOTE: the main/core method in your program should ALWAYS be called STATIC
+        // the RETURN of this method is VOID which means this method will NOT return a value
         System.out.println("Hello Beauitful World");
-    }
-}
+
+    };
+
+};
